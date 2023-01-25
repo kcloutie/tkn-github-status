6ee1eabaffdfd4a00ce9bfe5aea8d9bc4809aefa
kcloutie <kcloutie@ford.com>
2023-01-24 23:09:41 -0500

    Init

--- api/v1/pipelinerunstatus_types.go
+++ api/v1/pipelinerunstatus_types.go
@@ -20,22 +20,94 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type PipelineRunVariableSpec struct {
+
+	// The name of the pipelineRun variable that contains the github organization/owner name
+	OwnerVariable string `json:"ownerVariable,omitempty"`
+
+	// The name of the pipelineRun variable that contains the github repository name
+	RepoVariable string `json:"repoVariable,omitempty"`
+
+	// The name of the pipelineRun variable that contains the commit sha
+	ShaVariable string `json:"shaVariable,omitempty"`
+
+	// The name of the pipelineRun variable that contains the commit sha
+	// TargetUrlVariable string `json:"targetUrlVariable,omitempty"`
+
+}
+
+// SecretRef contains the information required to reference a single secret string
+// This is needed because the other secretRef types are not cross-namespace and do not
+// actually contain the "SecretName" field, which allows us to access a single secret value.
+type SecretRef struct {
+	SecretKey  string `json:"secretKey,omitempty"`
+	SecretName string `json:"secretName,omitempty"`
+}
 
 // PipelineRunStatusSpec defines the desired state of PipelineRunStatus
+// Link: https://docs.github.com/en/rest/commits/statuses?apiVersion=2022-11-28#create-a-commit-status
 type PipelineRunStatusSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// True or false whether the operator is disabled
+	// +optional
+	Disabled bool `json:"disabled,omitempty"`
 
-	// Foo is an example field of PipelineRunStatus. Edit pipelinerunstatus_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// A string label to differentiate this status from the status of other systems. This field is case-insensitive.
+	// +optional
+	Context string `json:"context,omitempty"`
+
+	// The base target URL to associate with this status. This URL will be linked from the GitHub UI to allow users to easily see the source of the status.
+	// +optional
+	TargetUrlBaseUri string `json:"targetUrlBaseUri,omitempty"`
+
+	// Pipeline variables extracted from the pipelineRun param section used to determine the owner/org name, repo name and commit sha of the pipelineRun
+	PipelineRunVariables PipelineRunVariableSpec `json:"pipelineRunVariables,omitempty"`
+
+	// The secret that contains the github personal access token used to make api calls to github
+	SecretRef *SecretRef `json:"secretRef,omitempty"`
+
+	GithubEnterprise string `json:"githubEnterprise,omitempty"`
 }
 
 // PipelineRunStatusStatus defines the observed state of PipelineRunStatus
 type PipelineRunStatusStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	PipelineRuns map[string]PipelineRunsStatus `json:"pipelineRuns,omitempty"`
+}
+
+type PipelineRunsStatus struct {
+	// The name of the pipeline run
+	PipelineRunName string `json:"pipelineRunName,omitempty"`
+
+	// Wether an error occurred processing the PipelineRun
+	HadError string `json:"hadError,omitempty"`
+
+	// The result of processing the PipelineRun
+	Result string `json:"result,omitempty"`
+
+	// The last record status of the pipelineRun
+	// +optional
+	LastStatus CommitStatusesStatus `json:"lastStatus,omitempty"`
+}
+type CommitStatusesStatus struct {
+	// The account owner of the repository
+	Owner string `json:"owner,omitempty"`
+
+	// The name of the repository
+	Repo string `json:"repo,omitempty"`
+
+	// The commit sha
+	Sha string `json:"sha,omitempty"`
+
+	// The state of the status
+	State string `json:"state,omitempty"`
+
+	// The target URL to associate with this status. This URL will be linked from the GitHub UI to allow users to easily see the source of the status
+	TargetUrl string `json:"targetUrl,omitempty"`
+
+	// A short description of the status
+	Description string `json:"description,omitempty"`
+
+	// A string label to differentiate this status from the status of other systems
+	Context string `json:"context,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- controllers/pipelinerunstatus_controller.go
+++ controllers/pipelinerunstatus_controller.go
@@ -18,14 +18,24 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	statusv1 "github.com/kcloutie/tkn-github-status/api/v1"
+	pipelineBeta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	statusv1 "github.com/kcloutie/tkn-github-status/api/v1"
 )
+
+const (
+	controllerLabelPrefix = "kcloutie.com"
+	kubeConfigEnvVar      = "KUBECONFIG"
+)
+
+var controllerConfigLabel = fmt.Sprintf("%s/tknGithubStatus", controllerLabelPrefix)
 
 // PipelineRunStatusReconciler reconciles a PipelineRunStatus object
 type PipelineRunStatusReconciler struct {
@@ -36,6 +46,8 @@ type PipelineRunStatusReconciler struct {
 //+kubebuilder:rbac:groups=status.kcloutie.com,resources=pipelinerunstatuses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=status.kcloutie.com,resources=pipelinerunstatuses/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=status.kcloutie.com,resources=pipelinerunstatuses/finalizers,verbs=update
+//+kubebuilder:rbac:groups="tekton.dev",resources=pipelineruns,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,11 +59,98 @@ type PipelineRunStatusReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *PipelineRunStatusReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	log.V(3).Info("Reconciling TektonHelperConfig")
+
+	var status statusv1.PipelineRunStatus
+	if err := r.Get(ctx, req.NamespacedName, &status); err != nil {
+		log.Error(err, "unable to get PipelineRunStatus", "namespace", req.Namespace, "name", req.Name)
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if status.Spec.Disabled {
+		return ctrl.Result{}, nil
+	}
+
+	foundPipelineRuns, err := r.listPipelineRuns(ctx, req.Namespace, status)
+	if err != nil {
+		return ctrl.Result{}, nil
+	}
+
+	if status.Status.PipelineRuns == nil {
+		status.Status.PipelineRuns = map[string]statusv1.PipelineRunsStatus{}
+	}
+
+	foundPipelineRunNames := map[string]pipelineBeta1.PipelineRun{}
+	// executeStatusUpdate := false
+
+	for _, item := range foundPipelineRuns {
+		shouldProcess := true
+
+		foundPipelineRunNames[string(item.UID)] = item
+
+		_, ok := status.Status.PipelineRuns[string(item.UID)]
+
+		if ok {
+			// Need to determine the status of the pipelineRun and only update the status if it is different than what we had set previously
+		} else {
+			// We have not yest set the status in github, lets set it
+			shouldProcess = true
+		}
+
+		if shouldProcess {
+
+		}
+	}
+
+	// toRemove := []string{}
+	// for key := range status.Status.Processed {
+	// 	_, ok := foundPipelineRunNames[key]
+	// 	if !ok {
+	// 		toRemove = append(toRemove, key)
+	// 	}
+	// }
+
+	// for _, item := range toRemove {
+	// 	delete(status.Status.Processed, item)
+	// 	executeStatusUpdate = true
+	// }
+	// if executeStatusUpdate {
+	// 	log.V(1).Info("Executing Status Update")
+	// 	if err := r.Status().Update(ctx, &config); err != nil {
+	// 		return ctrl.Result{}, fmt.Errorf("unable to update TektonHelperConfig status: %w", err)
+	// 	}
+	// }
 
 	return ctrl.Result{}, nil
+}
+
+func (r *PipelineRunStatusReconciler) listPipelineRuns(ctx context.Context, namespace string, status statusv1.PipelineRunStatus) (map[string]pipelineBeta1.PipelineRun, error) {
+	log := log.FromContext(ctx)
+
+	configNameLabelReq, _ := labels.NewRequirement(controllerConfigLabel, selection.Equals, []string{status.Name})
+	selector := labels.NewSelector()
+	selector = selector.Add(*configNameLabelReq)
+
+	listOps := &client.ListOptions{
+		LabelSelector: selector,
+		Namespace:     namespace,
+	}
+
+	foundPipelineRuns := map[string]pipelineBeta1.PipelineRun{}
+	pipelineRuns := &pipelineBeta1.PipelineRunList{}
+	err := r.List(ctx, pipelineRuns, listOps)
+	if err != nil {
+		log.Error(err, "failed to get the list of pipelineRuns", "namespace", namespace, "pipelineRunStatusName", status.Name)
+		return foundPipelineRuns, err
+	}
+
+	for _, item := range pipelineRuns.Items {
+		log.V(1).Info("Found a pipelineRun with the correct label value", "namespace", namespace, "pipelineRunStatusName", status.Name, "PipelineRun", item.Name, "label", controllerConfigLabel)
+		foundPipelineRuns[string(item.UID)] = item
+	}
+	return foundPipelineRuns, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
